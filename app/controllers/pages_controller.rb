256cfd2ee604a9625d48804d81786721b0670ea8
Walter <waltertan12@gmail.com>
2015-01-02 01:38:03 +0000

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,37 @@
+class PagesController < ApplicationController
+  def index
+  end
+  
+  def web_project
+  end
+
+  def pugs
+  end
+
+  def storybored
+  end
+
+  def colorbox
+  end
+
+  def roundtimer
+  end
+
+  def scroll
+  end
+
+  def dublindonutEP
+  end
+
+  def generator
+  end
+
+  def slidingmenus
+  end
+
+  def slider
+  end
+
+  def overlay
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get pugs" do
+    get :pugs
+    assert_response :success
+  end
+
+  test "should get storybored" do
+    get :storybored
+    assert_response :success
+  end
+
+  test "should get colorbox" do
+    get :colorbox
+    assert_response :success
+  end
+
+  test "should get roundtimer" do
+    get :roundtimer
+    assert_response :success
+  end
+
+  test "should get scroll" do
+    get :scroll
+    assert_response :success
+  end
+
+  test "should get dublindonutEP" do
+    get :dublindonutEP
+    assert_response :success
+  end
+
+  test "should get generator" do
+    get :generator
+    assert_response :success
+  end
+
+  test "should get slidingmenus" do
+    get :slidingmenus
+    assert_response :success
+  end
+
+  test "should get slider" do
+    get :slider
+    assert_response :success
+  end
+
+  test "should get overlay" do
+    get :overlay
+    assert_response :success
+  end
+
+end
