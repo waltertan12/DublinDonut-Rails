538207568fca1b0bec1831e2eba7f6130f632029
Walter <waltertan12@gmail.com>
2015-01-18 16:16:05 +0000

    Reverse order of poems in StoryBored

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,7 +1,8 @@
 class Poem < ActiveRecord::Base
     validates :poem, presence: true, length: {maximum: 500},
         uniqueness: {case_sensitive: false}
-        
+    default_scope -> { order(created_at: :desc) }
+    
     def create
         
     end
